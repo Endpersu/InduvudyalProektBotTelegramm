fb5ef1d43359a6eb4b5527be84e08fafbd98a833
Your Name <you@example.com>
2025-03-13 13:51:04 +0300

    bot

--- main.py
+++ main.py
@@ -1,28 +1,54 @@
 import os
 import asyncio
+from loguru import logger
+from dotenv import load_dotenv, find_dotenv
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
-from dotenv import find_dotenv, load_dotenv
+from aiogram.filters import Command
+
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 
-bot = Bot(TOKEN)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer("Это была команда старт")
-
-
-@dp.message()
-async def convers(message: types.Message):
-    if message.text.lower() == "утро":
-        await message.answer("Пьем кофе")
-
 
 async def main():
+    logger.add("file.log",
+               format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
+               rotation="3 days", backtrace=True, diagnose=True)
+
+    bot = Bot(TOKEN)
+    logger.info("Бот создан")
+    dp = Dispatcher()
+    logger.info("Диспетчер создан")
+
+    #@dp.message(Command('start'))
+    #async def send_welcome(message: types.Message):
+        #await message.answer("Здравствуйте! Какой тип подработки вас интересует? Я могу помочь найти вакансии в вашей области!")
+        #logger.info("Бот ответил на команду /start")
+
+    @dp.message(Command(""))
+    async def help(message: types.Message):
+        await message.answer("")
+        await message.answer("")
+        logger.info("Бот объяснил, что делает")
+
+    @dp.message(Command("start"))
+    async def cmd_start(message: types.Message):
+        kb = [
+            [types.KeyboardButton(text="Что ты умеешь?")],
+            [types.KeyboardButton(text="Найти подработку")]
+            [types.KeyboardButton(text='')]
+        ]
+        keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+        await message.answer("Здравствуйте! Какой тип подработки вас\
+                              интересует? Я могу помочь найти вакансии в вашей\
+                             области!", reply_markup=keyboard)
+
+    @dp.message()
+    async def echo(message: types.Message):
+        await message.answer(message.text)
+        logger.info(f"Бот вернул пользователю сообщение {message.text}")
+
     await dp.start_polling(bot)
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
